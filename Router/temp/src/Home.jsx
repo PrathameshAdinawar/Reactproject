26380d673d0530880cd4db5dbd003d520a5e7662
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2024-07-19 00:22:30 +0530

    adjust hogaya 992 tk

--- Router/temp/src/Home.jsx
+++ Router/temp/src/Home.jsx
@@ -9,10 +9,22 @@ const Home = () => {
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="10000">
-                        <div></div>
-                        <div className="Homebg1 ">
+
+                        <div className="homesmbg1  d-md-none d-flex justify-content-center">
+                            <div className="posism d-flex justify-content-center">
+                                <div className="wid ">
+                                    <h2 className="homeh2 smhh2 text-center ">Premiun Pet Foods</h2>
+                                    <p className="homep1 text-center w-100">Sed elementum tempus egestas sed sed risus pretium</p>
+                                    <div className="w-100  d-flex  justify-content-center mt-5">
+                                        <button className="homebtn " type="button">Shop Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="Homebg1 d-none d-md-block">
                             <div className=" container posi">
-                                <h2 className="homeh2 text-end ">Premiun Pet Foods</h2>
+                                <h2 className="homeh2 w-100 text-end ">Premiun Pet Foods</h2>
                                 <p className="homep1 text-end w-100">Sed elementum tempus egestas sed sed risus pretium</p>
                                 <div className="w-100  d-flex  justify-content-end mt-5">
                                     <button className="homebtn " type="button">Shop Now</button>
@@ -22,7 +34,20 @@ const Home = () => {
                     </div>
 
                     <div class="carousel-item" data-bs-interval="2000">
-                        <div className="Homebg2">
+
+                    <div className="homesmbg1 smbg2 d-md-none d-flex justify-content-center">
+                            <div className="posism d-flex justify-content-center">
+                                <div className="wid ">
+                                    <h2 className="homeh2 smhh2 text-center ">Natural Pet Foods</h2>
+                                    <p className="homep1 text-center w-100">Sed elementum tempus egestas sed sed risus pretium</p>
+                                    <div className="w-100  d-flex  justify-content-center mt-5">
+                                        <button className="homebtn " type="button">Shop Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="Homebg2 d-none d-md-block">
                             <div className="posi1">
                                 <h2 className="homeh2  ">Natural Pet Foods</h2>
                                 <p className="homep1 w-100">Sed elementum tempus egestas sed sed risus pretium</p>
@@ -34,7 +59,20 @@ const Home = () => {
                     </div>
 
                     <div class="carousel-item">
-                        <div className="Homebg3">
+
+                    <div className="homesmbg1 smbg3 d-md-none d-flex justify-content-center">
+                            <div className="posism d-flex justify-content-center">
+                                <div className="wid ">
+                                    <h2 className="homeh2 smhh2 text-center ">Natural Pet Foods</h2>
+                                    <p className="homep1 text-center w-100">Sed elementum tempus egestas sed sed risus pretium</p>
+                                    <div className="w-100  d-flex  justify-content-center mt-5">
+                                        <button className="homebtn " type="button">Shop Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="Homebg3 d-none d-md-block">
                             <div className="posi1">
                                 <h2 className="homeh2  ">Fresh Pet Foods</h2>
                                 <p className="homep1 w-100">Sed elementum tempus egestas sed sed risus pretium</p>
@@ -53,21 +91,23 @@ const Home = () => {
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+
             <div className="homebg4 ">
                 <div className="homebg5"></div>
-                <div className="d-flex justify-content-around">
+                <div className="d-lg-flex  justify-content-around container-md gap-3">
+
                     <div className="grid1 ">
                         <div className="ingrid position-absolute">
                             <h2 className="grid1h text-center">Nutritious Dog Food</h2>
-                            <div className="w-100 mt-5 mx-auto d-flex justify-content-center">
+                            <div className="w-100 mt-3 mx-auto d-flex justify-content-center">
                                 <button className="homebtn" type="button">Shop Now</button>
                             </div></div>
                     </div>
                     <div className="grid2 grid1">
                         <div className="ingrid1 position-absolute">
-                            <p className="ingrid1p">Save Upto 30%</p>
-                            <h2 className="grid1h ">Healthy Cat Food</h2>
-                            <div className="w-100 mt-5 mx-auto d-flex justify-content-center">
+                            <p className="ingrid1p text-center">Save Upto 30%</p>
+                            <h2 className="grid1h text-center">Healthy Cat Food</h2>
+                            <div className="w-100 mt-2 mx-auto d-flex justify-content-center">
                                 <button className="homebtn" type="button">Shop Now</button>
                             </div></div>
                     </div>
@@ -79,37 +119,37 @@ const Home = () => {
         </div>
         <div className="bgblk">
             <div>
-                <div className="container d-flex justify-content-around">
+                <div className="container d-md-flex justify-content-around">
                     <div col-2>
                         <img src="./public/footerlogo.png" alt="" />
                     </div>
-                    <div className="ms-5 ">
+                    <div className="ms-2 mt-4">
                         <input type="text" placeholder="Your Mail Here" className="ftp1 homeinpt" />
                         <button type="button" className="homebtn2 ms-2 ">Send</button>
-                    <div className="my-auto mt-3">
-                        <p className="ftp1 ">Join our list and get 15% off your first purchase!</p>
-                    </div> </div> 
+                        <div className="my-auto mt-3">
+                            <p className="ftp1 ">Join our list and get 15% off your first purchase!</p>
+                        </div> </div>
                 </div>
-                <div className="  mt-5 d-flex justify-content-around mb-5 container">
-                    <div className="d-flex justify-content-around ms-5 col-2 my-auto">
+                <div className="  mt-5 d-md-flex justify-content-around mb-5 container">
+                    <div className="d-flex justify-content-around  col-md-2  my-auto">
                         <div><img src="../public/twitter.png" alt="" /></div>
                         <div> <img src="../public/facebook.png" alt="" /></div>
                         <div><img src="../public/insta.png" alt="" /></div>
                         <div><img src="../public/youtube.png" alt="" /></div>
                     </div>
-                    <div col-2 className="my-auto w-25 ">
+                    <div  className="my-auto text-center mt-4">
                         <p className="ftp1">No: 58 A, East Madison Street,
                             Baltimore, MD, USA 4508</p></div>
-                    <div col-4 className="my-auto">
+                    <div className="my-auto text-center">
                         <p className="ftp1">+000 123 456789</p>
                     </div>
-                    <div className="my-auto">
+                    <div className="my-auto text-center">
                         <p className="ftp1">info@example.com</p>
                     </div>
-                    
-                    
+
+
                 </div>
-                <div className=" d-flex justify-content-center my-auto p-3 border-top">
+                <div className=" d-flex justify-content-center my-auto p-3 border-top text-center">
                     <p className="ftp1 mx-auto my-auto">© 2024 dt Petty (password: buddha) Design Themes</p>
                 </div>
             </div>
--- Router/temp/src/Navigation.jsx
+++ Router/temp/src/Navigation.jsx
@@ -1,40 +1,41 @@
 import { Link } from "react-router-dom"
 
-const Navigation=()=>{
-    
-    return(
+const Navigation = () => {
+
+    return (
         <div>
-            <nav className="navbar w-100 navbar-expand-lg" style={{height:"100px"}}>
-                <div className="d-flex justify-content-around h-100 container ">
-                <div className="col-2 col-sm-2 d-flex justify-content-center my-auto mx-auto ">
-                    <img src="../public/logo.png" alt="" className="" />
-                </div>
-                <ul className="d-lg-flex d-none justify-content-around col-8 fs-4 list-style-none text-decoration-none my-auto" style={{listStyle:"none"}}>
-                    <li><Link className="text-decoration-none text-dark" to={'/'}> Home</Link></li>
-                    <li><Link className="text-decoration-none text-dark" to={'./About'}>About</Link></li>
-                    <li><Link className="text-decoration-none text-dark" to={'./Shop'}>Shop</Link></li>
-                    <li><Link className="text-decoration-none text-dark" to={'./Reviews'}>Reviews</Link></li>
-                    <li><Link className="text-decoration-none text-dark" to={'./Contact'}>Contact</Link></li>
-                </ul>
-                <div className="d-flex justify-content-around col-2 col-sm-4 my-auto ">
-                    <div className="ps-5">
-                        <img src="../public/search.png" alt="" />
+            <nav className="navbar w-100 " style={{ height: "100px" }}>
+                <div className="d-flex justify-content-start h-100 container ">
+                    <div className="col-3 col-md-6 col-lg-2 d-flex justify-content-center my-auto mx-auto ">
+                        <img src="../public/logo.png" alt="" className="" />
                     </div>
-                    <div>
-                        <img src="../public/heart.png" alt="" />
-                    </div>
-                    <div>
-                        <img src="../public/cart.png" alt="" />
-                    </div>
-                </div>
-                <div className="col-sm-4">
-                   <button type="button" className="navhb">
-                    <img src="../public/hamburger.png" alt="" />
-                    </button>
-                    </div>
-                <div>
+                    <ul className="d-lg-flex d-none justify-content-around col-md-8 col-lg-7 fs-4 list-style-none text-decoration-none my-auto" style={{ listStyle: "none" }}>
+                        <li><Link className="text-decoration-none text-dark" to={'/'}> Home</Link></li>
+                        <li><Link className="text-decoration-none text-dark" to={'./About'}>About</Link></li>
+                        <li><Link className="text-decoration-none text-dark" to={'./Shop'}>Shop</Link></li>
+                        <li><Link className="text-decoration-none text-dark" to={'./Reviews'}>Reviews</Link></li>
+                        <li><Link className="text-decoration-none text-dark" to={'./Contact'}>Contact</Link></li>
+                    </ul>
+                    <div className="d-flex justify-content-evenly col-8 col-md-6 my-auto col-lg-3">
+                        <div className="ps-5">
+                            <img src="../public/search.png" alt="" />
+                        </div>
+                        <div>
+                            <img src="../public/heart.png" alt="" />
+                        </div>
+                        <div>
+                            <img src="../public/cart.png" alt="" />
+                        </div>
                     
-                </div>
+                        <div className="d-lg-none ">
+                            <button type="button" className="navhb ">
+                                <img src="../public/hamburger.png" alt="" />
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </nav>
         </div>

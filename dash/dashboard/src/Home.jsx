6b80674a9af0c22824a63182f57d386859f4c5e5
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2024-08-13 13:18:02 +0530

    Update Home.jsx

--- dash/dashboard/src/Home.jsx
+++ dash/dashboard/src/Home.jsx
@@ -1,17 +1,3 @@
-import { Box } from "@mui/material";
-import Sidemap from "./Sidemap";
-import Nav from "./Nav";
 
-function Home(){
-    return(
-        <>
-        <Nav/>
-        <Box sx={{ display: 'flex' }}>
-        <Sidemap/>
-        <h1>Home</h1>
-    </Box>
-    </>
-    )
-}
 
-export default Home;
+export default home;

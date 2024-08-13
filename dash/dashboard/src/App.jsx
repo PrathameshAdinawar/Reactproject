71fbacd3bd0ecbe10b70d6c5ef998448494e8254
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2024-08-13 13:18:38 +0530

    ok

--- dash/dashboard/src/App.jsx
+++ dash/dashboard/src/App.jsx
@@ -1,26 +1,11 @@
 
-import { Route,BrowserRouter,Routes } from "react-router-dom" 
-import Sidemap from "./Sidemap"
-import Home from "./Home"
-import Settings from "./Settings"
-import About from "./About"
-import Nav from "./Nav"
+import home from "./home"
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter>
       
-        <Routes>
-          <Route path="/" element={<Home />}></Route> 
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/settings" element={<Settings />}></Route>  
-          <Route path="/nav" element={<Nav />}></Route>  
-         
-          
-        </Routes>
-      </BrowserRouter>
     </>
   )
 }
